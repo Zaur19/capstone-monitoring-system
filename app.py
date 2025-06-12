4cbeb3b341866a1d472a071fc8e584b722f11969
Zaur19 <zaurshvangiradze26@gmail.com>
2025-06-12 18:08:38 +0400

    Removed app.run for Render compatibility

--- app.py
+++ app.py
@@ -139,9 +139,9 @@ def analyze():
 
     return jsonify(condition=condition, urgency=urgency, advice=advice)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=10000)
+# if __name__ == "__main__":
+#     app.run(debug=False, host="0.0.0.0", port=10000)
 
